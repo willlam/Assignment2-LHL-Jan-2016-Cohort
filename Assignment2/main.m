4c00c9dccb7eb9561891af7386b27525f58ab4bb
will <will@willlam.net>
2016-01-04 22:58:03 -0500

    Assignment #2

--- Assignment2/main.m
+++ Assignment2/main.m
@@ -8,10 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
-    return 0;
+
+@implementation NSArray (LargestNumber)
+
+
+int findLargestNumber (NSArray *numbers)
+{
+  int max = [[numbers valueForKeyPath:@"@max.intValue"] intValue];
+  return max;
 }
+
+
+int main(int argc, const char * argv[]) {
+  
+  NSArray *numbers = @[@22, @48, @86, @3, @200, @598];
+  NSArray *numbers2 = @[@21, @25, @44, @12345, @24, @5238];
+  
+  NSLog(@"Max of 1st array: %d", findLargestNumber(numbers));
+  NSLog(@"Max of 2nd array: %d", findLargestNumber(numbers2));
+  
+  
+  return 0;
+}
+
+@end
